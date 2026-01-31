b685a184e75f130d82d4a4d694fece2d8068a4f9
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-31 09:18:10 +0600

    added a sum function

---
+++ func3.js
@@ -0,0 +1,11 @@
+function myFunc (num1, num2, num3) {
+  console.log(num1 + num2 + num3)
+}
+
+myFunc(10, 20, 30)
+
+
+function num (gun1, gun2, gun3){
+  console.log(gun1 * gun2 * gun3)
+}
+num(2,2,2)
