f031b400fa5f7db23bcee4fcb87d76f698a3650c
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-31 20:20:48 +0600

    added a argument

---
+++ func5.js
@@ -0,0 +1,13 @@
+function num (num1){
+  console.log(num1)
+  let borgo = num1*num1
+  console.log(borgo)
+}
+num(5);
+num(12)
+
+function sum(number1, number2){
+  let result = number1 + number2
+  console.log(result)
+}
+sum(5,5)
