baef55bb850a7ea7b70201dcce929c735a9fad7b
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-31 08:38:51 +0600

    create a new function exercise

---
+++ func.js
@@ -0,0 +1,9 @@
+function myFunc (name, country) {
+  console.log("My name is " + name + " and I'm from " + country)
+  // console.log("My name is Saiful Islam and I'm from India")
+  // console.log("My name is Shakil Islam and I'm from Japan")
+  // console.log("My name is Sabbir Ahmed and I'm from Iran")
+  // console.log("My name is Rony Ahmed and I'm from USA")
+}
+myFunc("Towfiqul islam", "Bangladesh");
+//myFunc()
---
+++ func2.js
@@ -0,0 +1,5 @@
+function mySelf(name, district, fname, mname){
+  console.log("my name is " + name + " and I'm from " + district + " My Father's name is " + fname + " and my Mother name is " + mname)
+}
+
+mySelf("Towfiqul Islam", "Dhaka", "Md. Shah Amran", "Nasima Begum")
