eb94b9ad439d5e8d27d1792ec9d793e9ba793679
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-31 20:53:36 +0600

    added a func6

---
+++ func6.js
@@ -0,0 +1,14 @@
+function sum (num1, num2){
+  let result = num1 - num2
+  console.log(result)
+}
+let fAge = 55
+let mAge = 40
+sum(fAge, mAge)
+function double (number){
+  let sums = number * 2
+  console.log(sums)
+}
+double(5)
+double(6)
+double(7)
